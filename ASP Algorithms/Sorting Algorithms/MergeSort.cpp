19a39626de201cccbe4189cd9321d647ac6caf2b
Emir Veledar <emirveledar5@gmail.com>
2017-11-16 21:07:50 +0100

    Add files via upload

---
+++ ASP Algorithms/Sorting Algorithms/MergeSort.cpp
@@ -0,0 +1,63 @@
+﻿#include <iostream>
+using namespace std;
+
+void Merge(int Temp[], int Element[], const int list1, const int size1,
+	const int list2, const int size2)
+{
+	int i = list1, j = list2, k = list1;
+
+	while (i < list1 + size1 && j < list2 + size2)
+	{
+		if (Temp[i] < Temp[j])
+			Element[k++] = Temp[i++];
+		else
+			Element[k++] = Temp[j++];
+	}
+
+	while (i < list1 + size1)
+		Element[k++] = Temp[i++];
+	while (j < list2 + size2)
+		Element[k++] = Temp[j++];
+	for (k = list1; k < list1 + size1 + size2; k++)
+		Temp[k] = Element[k];
+}
+
+void MergeSort(int Temp[], int Element[], const int list1, const int size)
+{
+	int list2, size1, size2;
+
+	if (size > 1)
+	{
+		list2 = list1 + size / 2;
+		size1 = list2 - list1;
+		size2 = size - size1;
+		MergeSort(Temp, Element, list1, size1);
+		MergeSort(Temp, Element, list2, size2);
+		Merge(Temp, Element, list1, size1, list2, size2);
+	}
+}
+
+
+int main(){
+	
+	int
+		A[20] = { 25, 14, 7, 76, 10, 64, 20, 30, 9, 2, 6, 77, 83, 12, 56, 46, 61, 51, 33, 26 };
+	int
+		B[20] = { 25, 14, 7, 76, 10, 64, 20, 30, 9, 2, 6, 77, 83, 12, 56, 46, 61, 51, 33, 26 };
+
+	cout << "NESORTIRANI NIZ:" << endl;
+	for (int i = 0; i<20; i++)
+	{
+		cout << A[i] << endl;
+	}
+	MergeSort(B, A, 0, 20);
+	cout << endl << "SORTIRANI NIZ" << endl;
+	for (int i = 0; i<20; i++)
+	{
+		cout << B[i] << endl;
+	}
+
+
+	getchar();
+	return 0;
+}
---
+++ ASP Algorithms/Sorting Algorithms/QuickSort.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+void Swap(int N[], const int i, const int j)
+{
+	if (i != j)
+	{
+		int temp = N[i];
+		N[i] = N[j];
+		N[j] = temp;
+	}
+}
+
+void QuickSort(int N[], const int low, const int high)
+{
+	if (low < high)
+	{
+		int pivotloc = low;
+		int pivotkey = N[low];
+		for (int i = low + 1; i <= high; i++)
+		{
+			if (N[i] < pivotkey)
+			{
+				pivotloc++;
+				Swap(N, pivotloc, i);
+			}
+		}
+
+		Swap(N, low, pivotloc);
+
+		QuickSort(N, low, pivotloc - 1);
+		QuickSort(N, pivotloc + 1, high);
+	}
+}
+
+void ispis(int A[], int n)
+{
+	for (int i = 0; i<n; i++)
+	{
+		cout << A[i] << endl;
+	}
+}
+
+int main()
+{
+	int A[] = { 24, 13, 6, 75, 9, 63, 19, 20 };
+	int brE = 8;
+
+	cout << "NESORTIRANI NIZ:" << endl;
+	ispis(A, brE);
+
+	QuickSort(A, 0, brE - 1);
+
+	cout << endl << "SORTIRANI NIZ" << endl;
+	ispis(A, brE);
+
+
+
+	getchar();
+	return 0;
+}
