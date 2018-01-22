879f37896b0a2860c849762b5a29617a11248493
emir97 <emir.veledar@edu.fit.ba>
2018-01-22 21:35:45 +0100

    Druga Parcijala.

---
+++ ASP Algorithms/MatricaSusjedstva/Edge.h
@@ -0,0 +1,38 @@
+#ifndef EDGE_H
+#define EDGE_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Edge
+{
+
+	int vrhA;
+	int vrhB;
+	int tezina;
+public:
+	Edge()
+	{
+
+	}
+	Edge(int vrhA, int vrhB, int tezina)
+	{
+		this->vrhA = vrhA;
+		this->vrhB = vrhB;
+		this->tezina = tezina;
+	}
+	friend ostream& operator<<(ostream& o, Edge& i)
+	{
+		o << "t(" << i.vrhA << "," << i.vrhB << ") = " << i.tezina;
+		return o;
+	}
+
+	friend bool operator>(Edge& left, Edge& right)
+	{
+		return left.tezina < right.tezina;
+	}
+};
+
+#endif
---
+++ ASP Algorithms/MatricaSusjedstva/Main.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+using namespace std;
+
+#include "MatricaSusjedstva.h"
+
+void main(){
+	MatricaSusjedstva s(5);
+	int vrh = 2;
+	s.UcitajMatricuFromFile("C:\\Users\\Emir\\Desktop\\Matrica.txt");
+	s.ispisMatrice();
+	cout <<"Izlazni stepen vrha -> "<< s.izlazniStepen(vrh)<<endl;
+	cout << "Vrhovi koji su vrhu \"vrh\" susjedi -> ";
+	s.SusjediFrom(vrh)->Print();
+	cout << endl;
+	cout << "Vrhovi kojima je vrh \"vrh\"  susjed -> ";
+	s.SusjediTo(vrh)->Print();
+	cout << endl;
+	cout << "Najblizi susjed vrhu \"vrh\" -> " << s.najbliziSusjed(vrh)<<endl;
+	getchar();
+}
